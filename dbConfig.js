376923b77f45965b9f7711776f0cfe8aa1d9db22
Fantitom03 <leivafrancisco2017@gmail.com>
2024-11-26 00:50:20 -0300

    Comentario en la db config

--- dbConfig.js
+++ dbConfig.js
@@ -5,3 +5,6 @@ mongoose
   })
   .then(() => console.log('Conexión exitosa a MongoDB'))
   .catch((error) => console.error('Error al conectar a MongoDB: ', error));
+
+  //    useNewUrlParser: true,
+  //    useUnifiedTopology: true,
